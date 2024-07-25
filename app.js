8812fdbe228828887cc8d634880f21894f05d965
pascool001 <pascool404@gmail.com>
2024-07-25 09:13:20 +0000

    fixing default engine error

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'html');
 app.use(express.static(path.join(__dirname, "dist")));
 // app.use('/profile', express.static(path.join(__dirname, 'upload')));
 
